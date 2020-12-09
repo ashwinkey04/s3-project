c23bc32b770cc6ee907402c311a3d5e3b23dd5cd
Ashwin <ashwinkey04@gmail.com>
2020-12-09 23:36:05 +0530

    Add buyer space

--- src/main/ProjectDriver.java
+++ src/main/ProjectDriver.java
@@ -13,6 +13,7 @@ import javax.swing.LayoutStyle.ComponentPlacement;
 
 import view.AdminLogin;
 import view.BaseFrame;
+import view.BuyerSpace;
 import view.MusicianLogin;
 import view.SwingHelper;
 
@@ -42,9 +43,9 @@ public class ProjectDriver extends BaseFrame {
 	public ProjectDriver() {
 		super(); //Calls BaseFrame's constructor
 
-		JLabel lblProjectDriverManagement = new JLabel("Recording Company Mgmt");
-		lblProjectDriverManagement.setFont(SwingHelper.customFont(18));
-		lblProjectDriverManagement.setForeground(Color.GRAY);
+		JLabel lblMusicManagement = new JLabel("Recording Company Mgmt");
+		lblMusicManagement.setFont(SwingHelper.customFont(18));
+		lblMusicManagement.setForeground(Color.GRAY);
 
 		JButton btnAdminLogin = new JButton("Admin Login");
 		btnAdminLogin.addActionListener(new ActionListener() {
@@ -55,33 +56,50 @@ public class ProjectDriver extends BaseFrame {
 		});
 		btnAdminLogin.setFont(SwingHelper.customFont(15));
 
-		JButton btnLibrarianLogin = new JButton("Musician Login");
-		btnLibrarianLogin.addActionListener(new ActionListener() {
+		JButton btnMusicianLogin = new JButton("Musician Login");
+		btnMusicianLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				MusicianLogin.main(new String[] {});
 				frame.dispose();
 			}
 		});
-		btnLibrarianLogin.setFont(SwingHelper.customFont(15));
+		btnMusicianLogin.setFont(SwingHelper.customFont(15));
+		
+		JButton btnBuyer = new JButton("Buyer space");
+		btnBuyer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				BuyerSpace.main(new String[] {});
+				frame.dispose();
+			}
+		});
+		btnBuyer.setFont(SwingHelper.customFont(15));
+		
+		
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(gl_contentPane
 				.createSequentialGroup()
 				.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_contentPane.createSequentialGroup().addGap(64)
-								.addComponent(lblProjectDriverManagement))
+								.addComponent(lblMusicManagement))
 						.addGroup(gl_contentPane.createSequentialGroup().addGap(140)
 								.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
-										.addComponent(btnLibrarianLogin, Alignment.LEADING, GroupLayout.DEFAULT_SIZE,
+										.addComponent(btnMusicianLogin, Alignment.LEADING, GroupLayout.DEFAULT_SIZE,
 												GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 										.addComponent(btnAdminLogin, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 135,
+												Short.MAX_VALUE)
+										.addComponent(btnBuyer, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 135,
 												Short.MAX_VALUE))))
 				.addContainerGap(95, Short.MAX_VALUE)));
 		gl_contentPane
 				.setVerticalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_contentPane.createSequentialGroup().addContainerGap()
-								.addComponent(lblProjectDriverManagement).addGap(32)
+								.addComponent(lblMusicManagement).addGap(32)
 								.addComponent(btnAdminLogin, GroupLayout.PREFERRED_SIZE, 52, GroupLayout.PREFERRED_SIZE)
-								.addPreferredGap(ComponentPlacement.UNRELATED).addComponent(btnLibrarianLogin,
+								.addPreferredGap(ComponentPlacement.UNRELATED)
+								.addComponent(btnMusicianLogin,
+										GroupLayout.PREFERRED_SIZE, 53, GroupLayout.PREFERRED_SIZE)
+								.addPreferredGap(ComponentPlacement.UNRELATED)
+								.addComponent(btnBuyer,
 										GroupLayout.PREFERRED_SIZE, 53, GroupLayout.PREFERRED_SIZE)
 								.addContainerGap(70, Short.MAX_VALUE)));
 		contentPane.setLayout(gl_contentPane);
---
+++ src/view/BuyerSpace.java
@@ -0,0 +1,82 @@
+package view;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+import main.ProjectDriver;
+
+public class BuyerSpace extends BaseFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					frame = new BuyerSpace();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public BuyerSpace() {
+		super(); // Calls BaseFrame's constructor
+
+		setTitle("Buyer Space");
+		JLabel lblMusicianSection = new JLabel("Buyer Space");
+		lblMusicianSection.setFont(SwingHelper.customFont(20));
+		lblMusicianSection.setForeground(Color.GRAY);
+
+		JButton btnAddAlb = new JButton("Buy Album");
+		btnAddAlb.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				AddAlbum.main(new String[] {});
+				frame.dispose();
+			}
+		});
+		btnAddAlb.setFont(SwingHelper.customFont(15));
+
+		JButton btnLogout = new JButton("Back");
+		btnLogout.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				ProjectDriver.main(new String[] {});
+				frame.dispose();
+			}
+		});
+		
+		btnLogout.setFont(SwingHelper.customFont(15));
+
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane
+				.setHorizontalGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addGroup(gl_contentPane
+								.createSequentialGroup().addContainerGap(150, Short.MAX_VALUE)
+								.addComponent(lblMusicianSection, GroupLayout.PREFERRED_SIZE, 151,
+										GroupLayout.PREFERRED_SIZE)
+								.addGap(123))
+						.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup().addGap(134)
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addComponent(btnLogout, GroupLayout.PREFERRED_SIZE, 181,
+												GroupLayout.PREFERRED_SIZE)
+										.addComponent(btnAddAlb, GroupLayout.PREFERRED_SIZE, 181,
+												GroupLayout.PREFERRED_SIZE))
+								.addContainerGap(109, Short.MAX_VALUE)));
+		gl_contentPane.setVerticalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+						.addComponent(lblMusicianSection, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+						.addGap(18).addComponent(btnAddAlb, GroupLayout.PREFERRED_SIZE, 49, GroupLayout.PREFERRED_SIZE)
+						.addGap(18).addComponent(btnLogout, GroupLayout.PREFERRED_SIZE, 49, GroupLayout.PREFERRED_SIZE)
+						.addContainerGap(21, Short.MAX_VALUE)));
+		contentPane.setLayout(gl_contentPane);
+	}
+}
